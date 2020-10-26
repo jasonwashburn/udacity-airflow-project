928ea6c04d519c510d271fb4fe93b13645a3ebb7
Jason Washburn <wburn@wburn.net>
2020-10-26 12:12:33 -0500

    Initial Commit

---
+++ dags/udac_example_dag.py
@@ -0,0 +1,93 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': datetime(2019, 1, 12),
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *'
+        )
+
+#start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+# stage_events_to_redshift = StageToRedshiftOperator(
+#     task_id='Stage_events',
+#     dag=dag,
+#     table='public.staging_events',
+#     redshift_conn_id="redshift",
+#     aws_credentials_id="aws_credentials",
+#     s3_bucket="udacity-dend",
+#     s3_key="log_data",
+#     json_opts="s3://udacity-dend/log_json_path.json",
+#     destination_table_sql="""
+#         CREATE TABLE public.staging_events (
+#             artist varchar(256),
+#             auth varchar(256),
+#             firstname varchar(256),
+#             gender varchar(256),
+#             iteminsession int4,
+#             lastname varchar(256),
+#             length numeric(18,0),
+#             "level" varchar(256),
+#             location varchar(256),
+#             "method" varchar(256),
+#             page varchar(256),
+#             registration numeric(18,0),
+#             sessionid int4,
+#             song varchar(256),
+#             status int4,
+#             ts int8,
+#             useragent varchar(256),
+#             userid int4
+#         );
+#     """
+# )
+
+# stage_songs_to_redshift = StageToRedshiftOperator(
+#     task_id='Stage_songs',
+#     dag=dag
+# )
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag
+)
+
+# load_user_dimension_table = LoadDimensionOperator(
+#     task_id='Load_user_dim_table',
+#     dag=dag
+# )
+
+# load_song_dimension_table = LoadDimensionOperator(
+#     task_id='Load_song_dim_table',
+#     dag=dag
+# )
+
+# load_artist_dimension_table = LoadDimensionOperator(
+#     task_id='Load_artist_dim_table',
+#     dag=dag
+# )
+
+# load_time_dimension_table = LoadDimensionOperator(
+#     task_id='Load_time_dim_table',
+#     dag=dag
+# )
+
+# run_quality_checks = DataQualityOperator(
+#     task_id='Run_data_quality_checks',
+#     dag=dag
+# )
+
+# end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
