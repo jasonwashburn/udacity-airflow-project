00766589aed06e2ca022f1e2b5d4abbdfe28a31e
Jason Washburn <wburn@wburn.net>
2020-10-26 20:01:44 -0500

    Builds dimension tables

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -24,29 +24,29 @@ dag = DAG('udac_example_dag',
           catchup=False
         )
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+# start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
-stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag,
-    table='public.staging_events',
-    redshift_conn_id="redshift",
-    aws_credentials_id="aws_credentials",
-    s3_bucket="udacity-dend",
-    s3_key="log_data",
-    json_opts="s3://udacity-dend/log_json_path.json",
-)
+# stage_events_to_redshift = StageToRedshiftOperator(
+#     task_id='Stage_events',
+#     dag=dag,
+#     table='public.staging_events',
+#     redshift_conn_id="redshift",
+#     aws_credentials_id="aws_credentials",
+#     s3_bucket="udacity-dend",
+#     s3_key="log_data",
+#     json_opts="s3://udacity-dend/log_json_path.json",
+# )
 
-stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag,
-    table='public.staging_songs',
-    redshift_conn_id="redshift",
-    aws_credentials_id="aws_credentials",
-    s3_bucket="udacity-dend",
-    s3_key="song_data",
-    json_opts='auto',
-)
+# stage_songs_to_redshift = StageToRedshiftOperator(
+#     task_id='Stage_songs',
+#     dag=dag,
+#     table='public.staging_songs',
+#     redshift_conn_id="redshift",
+#     aws_credentials_id="aws_credentials",
+#     s3_bucket="udacity-dend",
+#     s3_key="song_data",
+#     json_opts='auto',
+# )
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
@@ -58,22 +58,38 @@ load_songplays_table = LoadFactOperator(
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='public.users',
+    append_mode=False,
+    sql_statement=SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='public.songs',
+    append_mode=False,
+    sql_statement=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='public.artists',
+    append_mode=False,
+    sql_statement=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='time',
+    append_mode=False,
+    sql_statement=SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
@@ -83,10 +99,10 @@ run_quality_checks = DataQualityOperator(
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-start_operator >> stage_events_to_redshift
-start_operator >> stage_songs_to_redshift
-stage_events_to_redshift >> load_songplays_table
-stage_songs_to_redshift >> load_songplays_table
+# start_operator >> stage_events_to_redshift
+# start_operator >> stage_songs_to_redshift
+# stage_events_to_redshift >> load_songplays_table
+# stage_songs_to_redshift >> load_songplays_table
 load_songplays_table >> load_song_dimension_table
 load_songplays_table >> load_user_dimension_table 
 load_songplays_table >> load_artist_dimension_table
--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -6,17 +6,39 @@ class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
+    insert_sql = """
+        INSERT INTO {}
+        {}
+    """
+
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_statement="",
+                 append_mode="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_statement = sql_statement
+        self.append_mode = append_mode
 
     def execute(self, context):
         self.log.info('LoadDimensionOperator not implemented yet')
+
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        if not self.append_mode:
+            self.log.info(f"Clearing {self.table} table.")
+            redshift.run(f"DELETE FROM {self.table}")
+
+        self.log.info(f"Building {self.table} table.")
+        formatted_sql =  LoadDimensionOperator.insert_sql.format(
+            self.table,
+            self.sql_statement
+        )
+
+        self.log.info(f"Executing sql: {formatted_sql}")
+        redshift.run(formatted_sql)
